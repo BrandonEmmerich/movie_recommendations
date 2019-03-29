ca6b8137eef4d25dac093c0a6b58538d815db08f
Brandon Emmerich <b@Brandons-Air.fios-router.home>
2019-03-29 18:08:11 -0400

    Refactor for Readability

--- recommend_movie.py
+++ recommend_movie.py
@@ -1,77 +1,72 @@
 import nltk
 from nltk.corpus import stopwords
+import numpy as np
 import pandas as pd
 import pprint
-import re
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-import string
 import sys
 
-def load_data():
+def load_and_clean_data():
     df_raw = pd.read_csv('data/raw_data.zip', compression='zip', header=0, sep=',', quotechar='"')
-    df = df_raw[df_raw['Origin/Ethnicity'] == 'American'].tail(1000)
+    df = df_raw[df_raw['Origin/Ethnicity'] == 'American']
+    df = df.fillna('.') # fill NA with punctuation so the TfidfVectorizer removes them
 
     return df
 
-def remove_unicode_and_nulls(input):
-    if type(input) == str:
-        clean_text = re.sub(r'[^\x00-\x7f]',r'', input)
-        return clean_text
-    elif input is None:
-        return '-'
-    else:
-        return input
+def calculate_cosine_similarity(search_parameter):
+    '''
+    Given a search parameter, ie a column in the movies dataframe, return cosine
+    similarity scores for a given movie with all other movies in the dataset.
+    '''
+    documents = df[search_parameter].tolist()
+    comparison_document = df[search_parameter][df['Title'] == user_input_movie_title].iloc[0]
+    documents.append(comparison_document)
+
+    tfidf = vectorizer.fit_transform(documents)
+    cosine_similarity_values = cosine_similarity(tfidf[-1], tfidf)
+
+    return cosine_similarity_values
+
 
 if __name__ == '__main__':
-    user_input = str(sys.argv[1])
+    user_input_movie_title = str(sys.argv[1])
+    df = load_and_clean_data()
     vectorizer = TfidfVectorizer()
-    df = load_data()
-    df = df.fillna('.')
-    df.applymap(remove_unicode_and_nulls)
+    all_movie_titles = df['Title'].tolist()
 
-    user_movie_index = df[df['Title'] == user_input].index.tolist()[0]
+    while True:
+        if user_input_movie_title not in all_movie_titles:
+            print("That movie isn't in the database, please check your spelling.")
+            break
 
-    movie_plots = df['Plot'].tolist()
-    plot_to_search = df['Plot'][df['Title'] == user_input].iloc[0]
-    movie_plots.append(plot_to_search)
-    tfidf_plots = vectorizer.fit_transform(movie_plots)
-    cosine_similarity_values_plots = cosine_similarity(tfidf_plots[-1], tfidf_plots)
+        search_parameters = ['Plot', 'Cast', 'Director']
+        cosine_similarity_values = []
+        for parameter in search_parameters:
+            values = calculate_cosine_similarity(parameter)
+            cosine_similarity_values.append(values)
 
-    movie_casts = df['Cast'].tolist()
-    cast_to_search = df['Cast'][df['Title'] == user_input].iloc[0]
-    movie_casts.append(cast_to_search)
-    tfidf_casts = vectorizer.fit_transform(movie_casts)
-    cosine_similarity_values_casts = cosine_similarity(tfidf_casts[-1], tfidf_casts)
+        average_cosine_similarty = np.array(cosine_similarity_values).mean(axis = 0)
+        sorted_similarity_scores = average_cosine_similarty.argsort()[0]
 
-    movie_directors = df['Director'].tolist()
-    director_to_search = df['Director'][df['Title'] == user_input].iloc[0]
-    movie_directors.append(director_to_search)
-    tfidf_directors = vectorizer.fit_transform(movie_directors)
-    cosine_similarity_values_directors = cosine_similarity(tfidf_directors[-1], tfidf_directors)
+        top_movie_positions = [-3, -4, -5, -6, -7] # Start counting at third to last because the list of plots includes the searched movie twice
 
-    average_cosine_similarty = (cosine_similarity_values_casts + cosine_similarity_values_plots + cosine_similarity_values_directors)/3.0
-    sorted_similarity_scores = average_cosine_similarty.argsort()[0]
+        movie_recommendations = []
+        for counter, position in enumerate(top_movie_positions):
+            movie_index = sorted_similarity_scores[position]
+            movie_details = df.iloc[sorted_similarity_scores[position]].to_dict()
+            movie_similarity_score = round(100 * average_cosine_similarty[0][movie_index], 1)
 
-    top_three = [-3, -4, -5] # Start counting at third to last because the list of plots includes the searched movie twice
+            recommendation = {
+                '1. Rank': counter + 1,
+                '2. Title': movie_details['Title'],
+                '3. Movie Similarity Score': movie_similarity_score,
+                '4. Cast': movie_details['Cast'].replace("\r\n", ", "),
+                '5. Director': movie_details['Director'],
+                '6. Year': movie_details['Release Year'],
+            }
+            movie_recommendations.append(recommendation)
 
-    movie_recommendations = []
-    for counter, position in enumerate(top_three):
-        movie_index = sorted_similarity_scores[position]
-        movie_details = df[df['Plot'] == movie_plots[movie_index]].to_dict()
-        percent_match = round(100*average_cosine_similarty[0][movie_index],1)
+        pprint.pprint(movie_recommendations)
 
-        recommendation = {
-            '1. Rank': counter + 1,
-            '2. Title': movie_details['Title'].values()[0],
-            '3. Percent Match': percent_match,
-            '4. Cast': movie_details['Cast'].values()[0].replace("\r\n", ", "),
-            '5. Director': movie_details['Director'].values()[0],
-            '6. Year': movie_details['Release Year'].values()[0],
-
-        }
-        movie_recommendations.append(recommendation)
-
-    pprint.pprint(movie_recommendations)
-
-import ipdb; ipdb.set_trace()
+        break
